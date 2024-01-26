b0c46db415cdc44cf5dc04e735413616584f0fbf
Anderson Lucena <obeloanarquista@gmail.com>
2024-01-26 10:58:26 -0300

    Add files via upload

---
+++ conversor_fahrenheit_celsius.py
@@ -0,0 +1,8 @@
+# Solicita a temperatura em Fahrenheit
+fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
+
+# Converte Fahrenheit para Celsius
+celsius = 5 * ((fahrenheit - 32) / 9)
+
+# Exibe a temperatura em Celsius
+print("A temperatura em graus Celsius é:", celsius)
